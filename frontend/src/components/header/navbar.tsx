57cb040f42bab4f6f721c0a31a2765b0f662b897
jeafreezy <jolaiyaemmanuel@gmail.com>
2025-09-02 04:41:42 +0200

    switched to headless ui for accessibility

--- frontend/src/components/header/navbar.tsx
+++ frontend/src/components/header/navbar.tsx
@@ -1,68 +1,118 @@
-import { APP_CONFIG } from "@/config/Content"
-import UseOSMLogo from '@/assets/useosm_logo.svg';
+import { APP_CONFIG } from "@/config/Content";
+import UseOSMLogo from "@/assets/useosm_logo.svg";
 import { APP_ROUTES } from "@/config/Routes";
 import { Button } from "@/components/ui/react/button";
-import { useState } from "react";
-
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
 export const NavBar = () => {
+  return (
+    <nav
+      className="flex px-14 py-4 justify-between items-center"
+      aria-label="Main navigation"
+    >
+      <div className="flex gap-x-40 items-center">
+    
+        <a href={APP_ROUTES.HOME}>
+          <img
+            src={UseOSMLogo.src}
+            alt="useOSM home"
+            loading="lazy"
+            decoding="async"
+          />
+        </a>
 
-    const [hoveredNav, setHoveredNav] = useState<string | null>(null);
+       
+        <ul className="flex gap-x-10" role="menubar">
+          {Object.keys(APP_CONFIG.NAVBAR).map((route) => {
+            const config = APP_CONFIG.NAVBAR[route];
+            const hasChildren = config.children.length > 0;
 
-    return (
-        <nav className="flex px-14 py-4 justify-between items-center">
-            <div className="flex gap-x-40 items-center">
-                <a href={APP_ROUTES.HOME}>
-                    <img src={UseOSMLogo.src} alt="useOSM navigation Logo" loading="lazy" decoding="async" />
-                </a>
-                <ul className="flex gap-x-10">
-                    {
-                        Object.keys(APP_CONFIG.NAVBAR).map(route =>
-                            <li
-                                key={`route-${route}`}
-                                className="relative"
-                            >
-                                <div
-                                    onMouseEnter={() => setHoveredNav(route)}
-                                    onMouseLeave={() => setHoveredNav(null)}
-                                    className="relative"
+            return (
+              <li key={`route-${route}`} role="none" className="relative">
+                {hasChildren ? (
+                  <Menu as="div" className="relative inline-block text-left">
+                    {({ open}) => (
+                      <div
+                        onMouseEnter={(e) => {
+                          // simulate click to open on hover
+                         (e.currentTarget.querySelector("button") as HTMLButtonElement)?.click();
+                        }}
+                        onMouseLeave={(e) => {
+                         if (open) { (e.currentTarget.querySelector("button") as HTMLButtonElement)?.click(); }
+                        }}
+                       
+                      >
+                        <MenuButton
+                          as={Button}
+                          size="lg"
+                          variant={open ? "navlinkHovered" : "navlink"}
+                          role="menuitem"
+                          id={`nav-link-${route}`}
+                          autoFocus={false}
+                         
+                      
+                        >
+                          {route}
+                        </MenuButton>
+                        
+                        <MenuItems
+                          className="absolute z-10 w-3xl origin-top-right bg-white border border-grey-50 rounded-2xl shadow-2xl focus:outline-none grid grid-cols-2 gap-x-2 p-4"
+                          aria-labelledby={`nav-link-${route}`}
+                          anchor='bottom'
+                        >
+
+                          {config.children.map((child) => (
+                            <MenuItem key={child.title}>
+                              {({ focus }) => (
+                                <a
+                                  href={child.route}
+                                  className={`${
+                                    focus ? "bg-white-2" : ""
+                                  } transition-colors duration-300 p-4 flex flex-col gap-2 rounded-2xl focus:outline-none`}
                                 >
-                                    <Button
-                                        variant={hoveredNav === route ? "navlinkHovered" : "navlink"}
-                                        id="nav-link-button"
-                                        size="lg" href={APP_CONFIG.NAVBAR[route].route}
-                                    >
-                                        {route}
-                                    </Button>
+                                  <h2 className="text-grey-400 font-semibold text-sm">
+                                    {child.title}
+                                  </h2>
+                                  <p className="text-xs text-grey-200">
+                                    {child.description}
+                                  </p>
+                                </a>
+                              )}
+                            </MenuItem>
+                          ))}
+                        </MenuItems>
+                      </div>
+                    )}
+                  </Menu>
+                ) : (
+             
+                  <Button
+                    href={config.route}
+                    size="lg"
+                    variant="navlink"
+                    role="menuitem"
+                    id={`nav-link-${route}`}
+                  >
+                    {route}
+                  </Button>
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
 
-                                    <div className={`${APP_CONFIG.NAVBAR[route].children.length > 0 && hoveredNav === route ? 'opacity-100 visible' : 'opacity-0 invisible'} transition-opacity duration-300 absolute z-10 translate-x-1/2 right-1/2 bg-white`}>
-                                        {/* This is acting as the gap between the nav and the children. This is to prevent the container from dissappearing when the cursor is not on the nav. */}
-                                        <span className="block w-full invisible h-4"></span>
-                                        <ul
-                                            className={`grid grid-cols-2 w-3xl gap-x-2 p-4 border border-grey-50 rounded-2xl shadow-2xl`}
-                                        >
-                                            {APP_CONFIG.NAVBAR[route].children.map((child) => (
-                                                <a className="text-grey-400 font-semibold text-sm" href={child.route} key={child.title}>
-                                                    <li  className="transition-colors duration-300 p-4 flex flex-col gap-2 rounded-2xl hover:bg-white-2">
-                                                        <h1 className="text-grey-400 font-semibold text-sm">{child.title}</h1>
-                                                        <p className="text-xs text-grey-200">{child.description}</p>
-                                                    </li>
-                                                </a>
-                                            ))}
-                                        </ul>
-                                    </div>
-                                </div>
-                            </li>
-                        )
-                    }
-                </ul>
-            </div>
-            <div>
-                {/* Todo - Add target=_blank to Button props */}
-                <Button id="nav-cta-button" size="lg" href="https://openstreetmap.org" target='_blank'>
-                    Get Started
-                </Button>
-            </div>
-        </nav>
-    )
-}
+      <div>
+        <Button
+          id="nav-cta-button"
+          size="lg"
+          href="https://openstreetmap.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Get Started
+        </Button>
+      </div>
+    </nav>
+  );
+};
